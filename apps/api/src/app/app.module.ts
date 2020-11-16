9ac1a418a8c6ff5f01b00851b818d2322a652070
João <joao.galiano.silva@gmail.com>
2020-11-16 10:26:03 +0000

    🔧 add ConfigModule into app.module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://user:pass@127.0.0.1:27017/db')],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.development.env' }),
+    MongooseModule.forRoot('mongodb://user:pass@127.0.0.1:27017/db'),
+  ],
 })
 export class AppModule {}
