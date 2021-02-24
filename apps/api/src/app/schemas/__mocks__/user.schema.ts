4812363ce9a9bebcbc46978362398ea5318be193
João <joao.lucas.89@hotmail.com>
2021-02-24 15:50:30 +0000

    🤡 mock userModel repository

--- apps/api/src/app/schemas/__mocks__/user.schema.ts
+++ apps/api/src/app/schemas/__mocks__/user.schema.ts
@@ -1,3 +1,4 @@
 import * as faker from 'faker';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const userMock = { faceitId: faker.random.uuid(), blacklistedPlayers: [] } as any;
+export const userRepositoryMock = { updateOne: jest.fn() };
