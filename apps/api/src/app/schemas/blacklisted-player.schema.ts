0925f230c79bfa21426d71b264b105d228e401c2
João <joao.lucas.89@hotmail.com>
2021-02-25 12:43:52 +0000

    ♻️ add schemas to graphql

--- apps/api/src/app/schemas/blacklisted-player.schema.ts
+++ apps/api/src/app/schemas/blacklisted-player.schema.ts
@@ -1,12 +1,16 @@
 import { Reason } from '@faceit-blacklist/interfaces';
-import { registerEnumType } from '@nestjs/graphql';
+import { Field, ID, ObjectType, registerEnumType } from '@nestjs/graphql';
 import { Prop, Schema } from '@nestjs/mongoose';
+@ObjectType()
 @Schema({ _id: false })
 export class BlacklistedPlayer {
+  @Field(() => ID)
   @Prop({ index: true, required: true })
   faceitId: string;
+  @Field(() => [Reason])
   @Prop({ type: [String], enum: Object.values(Reason), required: true })
   reason: Reason[];
+  @Field()
   @Prop()
   note?: string;
 }
