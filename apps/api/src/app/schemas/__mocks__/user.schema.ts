8796b3916908f169b4d4669e3028ec7e44cdfe65
João <joao.lucas.89@hotmail.com>
2021-02-25 12:50:40 +0000

    ♻️  remove unused mock

--- apps/api/src/app/schemas/__mocks__/user.schema.ts
+++ apps/api/src/app/schemas/__mocks__/user.schema.ts
@@ -1,7 +1,4 @@
 import * as faker from 'faker';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const userMock = { faceitId: faker.random.uuid(), blacklistedPlayers: [] } as any;
-export const userRepositoryMock = {
-  findOne: jest.fn(() => userMock),
-  findOneAndUpdate: jest.fn(() => userMock),
-};
+export const userRepositoryMock = { findOneAndUpdate: jest.fn(() => userMock) };
