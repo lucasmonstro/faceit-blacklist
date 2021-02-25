670c997f390618bcc7c046aafee701eea5228607
João <joao.lucas.89@hotmail.com>
2021-02-25 12:46:00 +0000

    ♻️ return user instead of boolean

--- apps/api/src/app/blacklist/resolvers/add-player-to-blacklist/add-player-to-blacklist.resolver.ts
+++ apps/api/src/app/blacklist/resolvers/add-player-to-blacklist/add-player-to-blacklist.resolver.ts
@@ -1,18 +1,19 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { User } from '../../../schemas/user.schema';
 import { CurrentUser } from '../../decorators/current-user.decorator';
 import { AddPlayerToBlacklistInput } from '../../inputs/add-player-to-blacklist.input';
 import { AddPlayerToBlacklistService } from '../../services/add-player-to-blacklist/add-player-to-blacklist.service';
-@Resolver(() => Boolean)
+@Resolver(() => User)
 export class AddPlayerToBlacklistResolver {
   constructor(
     private addPlayerToBlacklistService: AddPlayerToBlacklistService
   ) {}
-  @Mutation(() => Boolean)
+  @Mutation(() => User)
   async addPlayerToBlacklist(
     @CurrentUser('guid') ownerFaceitId: string,
     @Args('input') input: AddPlayerToBlacklistInput
-  ): Promise<true> {
-    return await this.addPlayerToBlacklistService.add(ownerFaceitId, input);
+  ): Promise<User> {
+    return this.addPlayerToBlacklistService.add(ownerFaceitId, input);
   }
   // TODO: remove when any query was added since graphql always require a root query
   @Query(() => Boolean)
