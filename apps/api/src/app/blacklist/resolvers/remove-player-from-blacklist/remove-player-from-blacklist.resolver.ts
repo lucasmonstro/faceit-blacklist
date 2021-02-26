11f96025221450eea1ce33211c70c9cb8a7e55fb
João <joao.lucas.89@hotmail.com>
2021-02-26 07:52:58 +0000

    ✨ add mutation to remove player from blacklist

---
+++ apps/api/src/app/blacklist/resolvers/remove-player-from-blacklist/remove-player-from-blacklist.resolver.ts
@@ -0,0 +1,20 @@
+import { Args, ID, Mutation, Resolver } from '@nestjs/graphql';
+import { User } from '../../../schemas/user.schema';
+import { CurrentUser } from '../../decorators/current-user.decorator';
+import { RemovePlayerFromBlacklistService } from '../../services/remove-player-from-blacklist/remove-player-from-blacklist.service';
+@Resolver(() => User)
+export class RemovePlayerFromBlacklistResolver {
+  constructor(
+    private removePlayerFromBlacklistService: RemovePlayerFromBlacklistService
+  ) {}
+  @Mutation(() => User)
+  async removePlayerFromBlacklist(
+    @CurrentUser('guid') ownerFaceitId: string,
+    @Args('faceitId', { type: () => ID }) faceitIdToRemove: string
+  ): Promise<User> {
+    return await this.removePlayerFromBlacklistService.remove(
+      ownerFaceitId,
+      faceitIdToRemove
+    );
+  }
+}
---
+++ apps/api/src/app/blacklist/services/remove-player-from-blacklist/remove-player-from-blacklist.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDoc } from '../../../schemas/user.schema';
+@Injectable()
+export class RemovePlayerFromBlacklistService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<UserDoc>
+  ) {}
+  async remove(
+    ownerFaceitId: string,
+    faceitIdToRemove: string
+  ): Promise<User> {
+    return await this.userModel.findOneAndUpdate(
+      { faceitId: ownerFaceitId },
+      { $pull: { blacklistedPlayers: { faceitId: faceitIdToRemove } } },
+      { new: true }
+    );
+  }
+}
