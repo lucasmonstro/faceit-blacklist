8e5a03df3ab865547d9cd13ef501ff1e93c3db67
João <joao.lucas.89@hotmail.com>
2021-02-26 09:26:20 +0000

    ✅ update tests with $addToSet

--- apps/api/src/app/blacklist/services/add-player-to-blacklist/add-player-to-blacklist.service.spec.ts
+++ apps/api/src/app/blacklist/services/add-player-to-blacklist/add-player-to-blacklist.service.spec.ts
@@ -34,11 +34,8 @@ describe('AddPlayerToBlacklistService', () => {
     const input = createAddPlayerToBlacklistInput();
     expect(await service.add(ownerFaceitId, input)).toBe(userMock);
     expect(userRepositoryMock.findOneAndUpdate).toHaveBeenCalledWith(
-      {
-        faceitId: ownerFaceitId,
-        'blacklistedPlayers.faceitId': { $ne: input.faceitId },
-      },
-      { $push: { blacklistedPlayers: input } },
+      { faceitId: ownerFaceitId },
+      { $addToSet: { blacklistedPlayers: input } },
     );
   });
 });
