9fa45f774edd16c5d2b2193df972ec3279cda260
João <joao.lucas.89@hotmail.com>
2021-02-28 10:16:13 +0000

    ♻️ remove feature add player to blacklist

--- apps/api/src/app/blacklist/blacklist.module.ts
+++ apps/api/src/app/blacklist/blacklist.module.ts
@@ -1,10 +1,8 @@
 import { HttpModule, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../schemas/user.schema';
-import { AddPlayerToBlacklistResolver } from './resolvers/add-player-to-blacklist/add-player-to-blacklist.resolver';
 import { GetCurrentUserResolver } from './resolvers/get-current-user/get-current-user.resolver';
 import { RemovePlayerFromBlacklistResolver } from './resolvers/remove-player-from-blacklist/remove-player-from-blacklist.resolver';
-import { AddPlayerToBlacklistService } from './services/add-player-to-blacklist/add-player-to-blacklist.service';
 import { GetUserService } from './services/get-user/get-user.service';
 import { RemovePlayerFromBlacklistService } from './services/remove-player-from-blacklist/remove-player-from-blacklist.service';
 import { IsFaceitIdValidator } from './validators/is-faceit-id.validator';
@@ -14,8 +12,6 @@ import { IsFaceitIdValidator } from './validators/is-faceit-id.validator';
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   providers: [
-    AddPlayerToBlacklistService,
-    AddPlayerToBlacklistResolver,
     GetUserService,
     GetCurrentUserResolver,
     RemovePlayerFromBlacklistService,
